7f87abfd65e6568d4d1521262fab837f24574db6
AdminAkai <akatoirotech@gmail.com>
2026-08-11 17:46:18 -0400

    Added search and pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,6 +13,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebounceCallback((e: ChangeEvent<HTMLInputElement>) => {
     const term = e.target.value
     const params = new URLSearchParams(searchParams)
+
+    params.set('page', '1')
+
     if (term) {
       params.set('query', term);
     } else {
