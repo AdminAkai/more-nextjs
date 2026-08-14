d4ffb7e71def14ebb139a82f3bc7a43d61a29c8a
AdminAkai <akatoirotech@gmail.com>
2026-08-14 16:07:40 -0400

    Added error messaging for all inputs in create invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,11 +13,11 @@ const invoicesURL = '/dashboard/invoices'
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
-    invalid_type_error: 'Please select a customer.'
+    required_error: 'Please select a customer.'
   }),
   amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than 0.'}),
   status: z.enum(['pending', 'paid'], {
-    invalid_type_error: 'Please select an invoice status.'
+    required_error: 'Please select an invoice status.'
   }),
   date: z.string()
 })
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -18,9 +18,11 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} }
   const [state, formAction] = useActionState(createInvoice, initialState)
 
+  console.log(state)
+
   return (
     <form action={formAction}>
-      <div className="rounded-md bg-gray-50 p-4 md:p-6">
+      <div className="rounded-md bg-gray-50 p-4 md:p-6" aria-describedby='form-error'>
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -46,7 +48,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
           <div id="customer-error" aria-live="polite" aria-atomic="true">
-              {state.errors?.customerId && state.errors.customerId.map(error: string => (
+              {state.errors?.customerId && state.errors.customerId.map(error => (
                 <p className="mt-2 text-sm text-red-500" key={error}>
                   {error}
                 </p>
@@ -68,15 +70,23 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby='amount-error'
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+          </div>
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+              {state.errors?.amount && state.errors.amount.map(error => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
         {/* Invoice Status */}
         <fieldset>
-          <legend className="mb-2 block text-sm font-medium">
+          <legend className="mb-2 block text-sm font-medium" aria-describedby='status-error'>
             Set the invoice status
           </legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
@@ -113,7 +123,21 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
           </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status && state.errors.status.map(error => (
+              <p className="mt-2 text-sm text-red-500" key={error}>
+                {error}
+              </p>
+            ))}
+          </div>
         </fieldset>
+        <div id='form-error' aria-live="polite" aria-atomic="true">
+          {state.message &&
+            <p className="mt-2 text-sm text-red-500" key={state.message}>
+              {state.message}
+            </p>
+          }
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
