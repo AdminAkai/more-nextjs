e1b9ba023a6330d300a9653e98f50536911e3373
AdminAkai <akatoirotech@gmail.com>
2026-08-14 03:52:26 -0400

    Added zod validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,9 +12,13 @@ const invoicesURL = '/dashboard/invoices'
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.'
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Please enter an amount greater than 0.'}),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.'
+  }),
   date: z.string()
 })
 
@@ -25,11 +29,29 @@ const redirectAndRevalidateInvoices = () => {
   redirect(invoicesURL)
 }
 
-export const createInvoice = async (formData: FormData) => {
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  }
+  message?: string | null;
+}
+
+export const createInvoice = async (_: State, formData: FormData): Promise<State> => {
   const rawData = extractFormData(formData)
 
   try {
-    const { customerId, amount, status } = InvoiceValidation.parse(rawData)
+    const validatedFields = InvoiceValidation.safeParse(rawData)
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing fields. Failed to create invoice.'
+      }
+    }
+
+    const { customerId, amount, status } = validatedFields.data
 
     const amountInCents = amount * 100
     const date = new Date().toISOString().split('T')[0]
@@ -40,9 +62,14 @@ export const createInvoice = async (formData: FormData) => {
     `
   } catch (err) {
     console.error(err)
+    return {
+      message: 'Database error: Failed to create invoice.'
+    }
   }
 
   redirectAndRevalidateInvoices()
+
+  return {}
 }
 
 export const updateInvoice = async (id: string, formData: FormData) => {
